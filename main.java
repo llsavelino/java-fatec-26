7ccab3a4a03cc2c6d76edb7188191588c0a7286a
Lino <savelino852@gmail.com>
2026-03-07 10:10:55 -0300

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Conta conta1 = new Conta(1234, 567, "João Silva");
+        ArCondicionado ac = new ArCondicionado("Samsung", "Eco", 22);
 
         conta1.depositar(500);
         conta1.sacar(200);
@@ -12,5 +13,16 @@ public class Main {
         conta1.encerrarConta();
 
         System.out.println(conta1);
+
+        ac.ajustarTemperatura(10);
+        ac.ajustarTemperatura(25);
+        
+        System.out.println(ac);
+        
+        ac.modoTurbo();
+        ac.ligar();
+        ac.modoTurbo();
+        
+        System.out.println(ac);
     }
 }
