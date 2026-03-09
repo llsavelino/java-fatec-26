33f08f599f62cc1e3e67b31842b23170fa6623ed
Lino <savelino852@gmail.com>
2026-03-09 18:07:25 -0300

    Update main.java

--- main.java
+++ main.java
@@ -3,6 +3,8 @@ public class Main {
 
         Conta conta1 = new Conta(1234, 567, "João Silva");
         ArCondicionado ac = new ArCondicionado("Samsung", "Eco", 22);
+        Placar placar = new Placar("Corinthians", "Flamengo");
+        Streaming streaming = new Streaming("Alguém", "Premium");
 
         conta1.depositar(500);
         conta1.sacar(200);
@@ -24,5 +26,11 @@ public class Main {
         ac.modoTurbo();
         
         System.out.println(ac);
+
+        placar.registrarPonto("Corinthians", 3);
+        System.out.println(placar);
+
+        streaming.assistirFilme("Star Wars");
+        System.out.println(streaming);
     }
 }
